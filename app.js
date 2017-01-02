f9f6205ce48f56ad9a1c007a886f99764a720a69
smile86 <lovelyeu@naver.com>
2017-01-02 17:24:26 +0900

    hello message

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname);
+app.use(express.static(path.join(__dirname + '/public')));
+//app.use(express.static(__dirname + '/public'));
+// path 를 사용하면 /public에서 '/' 상관없이 주소조합해줌
+// 그러나 지금 path를 사용하니까  브라우저에 Cannot GET / 라는 메시지가 나옴
+// 더 해보니까 /public 이라고 해주니까 똑같이 나옴.
+// 그렇다는 것은 path를 왜 쓰는지 모르겠음
+//console.log(__dirname);
 app.listen(3000, function(){
 	console.log('Server On');
 });
